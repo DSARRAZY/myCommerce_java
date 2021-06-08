fae4fef83c6d73ae84cb31c354e026ec30b99335
DSARRAZY <d.sarrazy@gmail.com>
2021-06-08 17:07:09 +0200

    ajout et affichage d'un produit

--- src/main/java/com/example/mycommerce/ListProductServlet.java
+++ src/main/java/com/example/mycommerce/ListProductServlet.java
@@ -2,6 +2,7 @@ package com.example.mycommerce;
 
 import com.example.mycommerce.MyProductDao;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,24 +16,35 @@ import java.util.List;
 @WebServlet(urlPatterns = "/listProduct")
 public class ListProductServlet extends HttpServlet {
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<MyProduct> products  = MyProductDao.getAllProducts();
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//    }
 
-        PrintWriter out = resp.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Afficher liste produit</title>");
-        out.println("</head>");
-        out.println("<body>");
-        for (MyProduct p : products ) {
-            out.println("<p>Les produits " + p.getName() + p.getContent() + p.getPrice() +" !</p>");
-        }
 
-        HttpSession session = req.getSession();
-        resp.getWriter().println("Mon nom est :"+session.getAttribute("user"));
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<MyProduct> products = MyProductDao.getAllProducts();
+        req.setAttribute("products", products);
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/listProduct.jsp");
+        rd.forward(req, resp);
 
-        out.println("</body>");
-        out.println("</html>");
     }
 }
+
+
+//        PrintWriter out = resp.getWriter();
+//        out.println("<html>");
+//        out.println("<head>");
+//        out.println("<title>Afficher liste produit</title>");
+//        out.println("</head>");
+//        out.println("<body>");
+//        for (MyProduct p : products ) {
+//            out.println("<p>Les produits " + p.getName() + p.getContent() + p.getPrice() +" !</p>");
+//        }
+//
+//        HttpSession session = req.getSession();
+//        resp.getWriter().println("Mon nom est :"+session.getAttribute("user"));
+//
+//        out.println("</body>");
+//        out.println("</html>");
+
+
