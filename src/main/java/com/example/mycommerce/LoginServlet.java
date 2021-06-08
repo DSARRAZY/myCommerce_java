5ce244e5977ffa2734aab6f0788d2e6d17190d4b
DSARRAZY <d.sarrazy@gmail.com>
2021-06-08 13:35:38 +0200

    rajout auth

--- src/main/java/com/example/mycommerce/LoginServlet.java
+++ src/main/java/com/example/mycommerce/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.example.mycommerce;
 
 import javax.net.ssl.HandshakeCompletedEvent;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,15 +14,10 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
-
         HttpSession session = req.getSession();
-
-
-
-        session.setAttribute("name", name);
-
-
+        session.setAttribute("user", req.getParameter("name"));
+        RequestDispatcher rd = req.getRequestDispatcher("/web-app/login.html");
+        rd.forward(req, resp);
 
     }
 }
