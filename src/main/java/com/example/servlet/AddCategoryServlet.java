57b4155811796d66455ed0dfe34caf0d35d80cf1
DSARRAZY <d.sarrazy@gmail.com>
2021-06-10 11:51:57 +0200

    correction persistence.xml

--- src/main/java/com/example/servlet/AddCategoryServlet.java
+++ src/main/java/com/example/servlet/AddCategoryServlet.java
@@ -18,52 +18,56 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/auth/addCategorie")
 public class AddCategoryServlet extends HttpServlet {
 
-//    @Override
-//    public void init() throws ServletException {
-//        EntityManagerFactory emf = null;
-//        emf = Persistence.createEntityManagerFactory("My-PU");
-//        EntityManager em = emf.createEntityManager();
-//        Category category= em.find(Category.class, 1);
-//        em.close();
-//        emf.close();
-//    }
-//
-//    @Override
-//    public void destroy() {
-//        Process em = null;
-//        em.destroy();
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Category category= new Category();
-//        category.setName(req.getParameter("name"));
-//
-//
-//
-//        EntityManager em = this.emf.createEntityManager();
-//        EntityTransaction et = em.getTransaction();
-//        et.begin();
-//        try {
-//            Category category = em.find(Category.class, 1);
-//            em.remove(category);
-//            et.commit();
-//        } catch (RuntimeException re) {
-//            if (et.isActive())
-//                et.rollback();
-//        }finally {
-//            em.close();
-//        }
-//
-//
-//        resp.sendRedirect( req.getContextPath() + "/auth/listProduct");
-//
-//
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/addCategory.jsp");
-//        rd.forward(req, resp);
-//    }
+    @Override
+    public void init() throws ServletException {
+
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("My-PU");
+        EntityManager entityManager = emfactory.createEntityManager();
+        entityManager.getTransaction( ).begin( );
+
+        Category category= em.find(Category.class, 1);
+
+        entityManager.persist(category);
+        entityManager.getTransaction( ).commit( );
+
+        entityManager.close( );
+        emfactory.close( );
+    }
+
+    @Override
+    public void destroy() {
+        Process em = null;
+        em.destroy();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Category category= new Category();
+        category.setName(req.getParameter("name"));
+
+        EntityManager em = this.emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            Category category = em.find(Category.class, 1);
+            em.remove(category);
+            et.commit();
+        } catch (RuntimeException re) {
+            if (et.isActive())
+                et.rollback();
+        }finally {
+            em.close();
+        }
+
+
+        resp.sendRedirect( req.getContextPath() + "/auth/listProduct");
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/addCategory.jsp");
+        rd.forward(req, resp);
+    }
 }
